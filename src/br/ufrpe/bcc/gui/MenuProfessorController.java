1ab686c7aeca2be2aff1bd4390fefc7726b65366
Rickypool <squall_leonarth.ralf@hotmail.com>
2018-08-06 18:17:17 -0300

    Alterações + Salvando arquivo.

---
+++ src/br/ufrpe/bcc/gui/MenuProfessorController.java
@@ -0,0 +1,20 @@
+package br.ufrpe.bcc.gui;
+
+import javafx.fxml.FXML;
+
+public class MenuProfessorController {
+
+    @FXML
+    void irAlocacaoSala() {
+        Telas.getInstance().getNovaAlocacaoSala();;
+    }
+
+    @FXML
+    void irSalasAlocadas() {
+        Telas.getInstance().getAlocarSala();
+    }
+
+    void sair() {
+        Telas.getInstance().getMenuProfessor();
+    }
+}
